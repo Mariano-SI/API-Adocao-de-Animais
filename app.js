8a2016ea068052a75ec2c972a87e874e1e92dcac
Mariano Silva <mariano.silva@dtidigital.com>
2023-06-25 21:12:59 -0300

    admin e animal ok

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//rotas importadas de router
+app.use('/animais', require('./Routes/animalRoutes'))
+app.use('/admin', require('./Routes/adminRoutes'))
+
+const server = app.listen(3000, function(){
+   const host = server.address().address;
+   const port = server.address().port;
+   console.log(`Servidor iniciado em http://localhost:${port}`)
+});
