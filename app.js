b5a12235bef963132e778381a4d2bdb06e9eb819
Mariano Carlos Silva <76002553+Mariano-SI@users.noreply.github.com>
2023-06-27 13:56:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/admin', require('./Routes/adminRoutes'))
 app.use('/adotantes', require('./Routes/adotanteRoutes'))
 app.use('/resgates', require('./Routes/resgateRoutes'))
 
-const server = app.listen(3000, function(){
+const server = app.listen(3000,'0.0.0.0', function(){
    const host = server.address().address;
    const port = server.address().port;
    console.log(`Servidor iniciado em http://localhost:${port}`)
