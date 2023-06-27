c4ce17e1a6c9a3a34419905184ae93a2fa811f6e
Mariano Carlos Silva <76002553+Mariano-SI@users.noreply.github.com>
2023-06-27 13:30:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
+const cors = require("cors");
+
+const issue2options = {
+  origin: true,
+  methods: ["POST", "GET", "HEAD", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  credentials: true,
+  maxAge: 3600,
+  exposedHeaders: ["Total-Count", "Unread-Count"],
+};
+
+app.use(cors(issue2options));
+app.options("*", cors());
 
 
 app.use(bodyParser.urlencoded({extended:false}));
