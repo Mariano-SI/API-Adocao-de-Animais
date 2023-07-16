82fa71328a9f97e9a24ad96f3ea6c37385fd98d0
Lucas Alexsander Barbosa Cruz <lucas10barbosa_@hotmail.com>
2023-07-16 11:35:25 -0300

    Alteração no callback

--- Model/Animal.js
+++ Model/Animal.js
@@ -1,107 +1,108 @@
 //importa o modulo do mysql
 const mysql = require('mysql');
 
-class CadAnimal{
-    //Função para conectar o BD
-    static connect(){
-        //Cria conexao
-        const connection = mysql.createConnection({
-            host:'bd2-ufvjm.mysql.database.azure.com',
-            user:'Mariano',
-            password:'m-88443244',
-            database:'trabbd',
-        });
-        //Conecta ao banco
-        connection.connect();
-        return connection;
+class CadAnimal {
+  //Função para conectar o BD
+  static connect() {
+    //Cria conexao
+    const connection = mysql.createConnection({
+      host: '127.0.0.1',
+      user: 'root',
+      password: '',
+      database: 'onganimal',
+    });
+    //Conecta ao banco
+    connection.connect();
+    return connection;
+  }
+  //Retorna lista de carros
+  static getAnimal(res) {
+    const connection = CadAnimal.connect();
+    connection.beginTransaction();
+    try {
+      const sql = 'select * from animal';
+
+      const query = connection.query(sql, function (error, results, fields) {
+        if (error) throw error;
+        return res.status(200).send(results);
+      });
+      connection.commit();
+    } catch (error) {
+      connection.rollback();
+      throw new Error('Não foi possivel concluir a transação', 500);
+    } finally {
+      connection.end();
     }
-    //Retorna lista de carros
-    static getAnimal(res){
-        const connection = CadAnimal.connect();
-        connection.beginTransaction();
-        try {
-            const sql = "select * from cadAnimal";
+  }
 
-            const query = connection.query(sql, function(error, results, fields){
-                if(error) throw error;
-                return res.status(200).send(results);
-            });
-            connection.commit();
-        } catch (error) {
-            connection.rollback();
-            throw new Error("Não foi possivel concluir a transação", 500);
-        }finally{
-            connection.end();
-        }
+  //Retorna a lista de carros por tipo de banco de dado
+  static getAnimalById(id, callback) {
+    const connection = CadAnimal.connect();
+
+    //Consulta
+    const sql = `select * from animal where animal.id = ${id}`;
+    const query = connection.query(sql, function (error, results, fields) {
+      if (error) throw error;
+      //Retorna os dados pela callback
+      callback(results);
+    });
+    console.log(query.sql);
+    connection.end();
+  }
+
+  //Atualiza o carro no BD
+  static updateAnimal(id, { nome, especie, porte, sexo, idade }, res) {
+    const connection = CadAnimal.connect();
+    connection.beginTransaction();
+    try {
+      const setStatementCollumns = []; //copiei da Round, gostei.
+
+      if (nome) {
+        setStatementCollumns.push(`nome = '${nome}'`);
+      }
+      if (especie) {
+        setStatementCollumns.push(`especie = '${especie}'`);
+      }
+      if (porte) {
+        setStatementCollumns.push(`porte = '${porte}'`);
+      }
+      if (sexo) {
+        setStatementCollumns.push(`sexo = '${sexo}'`);
+      }
+      if (idade) {
+        setStatementCollumns.push(`idade = ${idade}`);
+      }
+
+      const sql = `UPDATE animal SET ${setStatementCollumns.join(
+        ',',
+      )} where id = ${id}`;
+      const query = connection.query(sql, function (error, results, fields) {
+        if (error) throw error;
+        return res.status(200).send(results);
+      });
+
+      connection.commit();
+    } catch (error) {
+      connection.rollback();
+      throw new Error('Erro com o servidor', 500);
+    } finally {
+      connection.end();
     }
+  }
 
-    //Retorna a lista de carros por tipo de banco de dado
-    static getAnimalById(id, callback){
-        const connection = CadAnimal.connect();
+  static createAnimal(animal, callback) {
+    const connection = CadAnimal.connect();
+    const { nome, especie, porte, sexo, idade } = animal;
 
-        //Consulta
-        const sql= `select * from cadAnimal left join resgateAnimal on cadAnimal.id = resgateAnimal.id_animal left join adocao on cadAnimal.id = adocao.id_animal where cadAnimal.id = ${id}`;
-        const query = connection.query(sql, function(error, results, fields){
-            if(error) throw error;
-            //Retorna os dados pela callback
-            callback(results);
-        });
-        console.log(query.sql);
-        connection.end();
-    }
+    const sql = `INSERT INTO animal ( nome, especie, porte, sexo, idade ) values ( '${nome}', '${especie}', '${porte}', '${sexo}', ${idade})`;
+    const query = connection.query(sql, function (error, results, fields) {
+      if (error) throw error;
+    });
 
-        //Atualiza o carro no BD
-        static updateAnimal(id, {nome, especie, porte, sexo, idade, descricao}, callback){
-            const connection = CadAnimal.connect();
-
-            const setStatementCollumns = [] //copiei da Round, gostei.
-
-            if(nome){
-                setStatementCollumns.push(`nome = '${nome}'`)
-            }
-            if(especie){
-                setStatementCollumns.push(`especie = '${especie}'`)
-            }
-            if (porte) {
-                setStatementCollumns.push(`porte = '${porte}'`)
-            }
-            if(sexo){
-                setStatementCollumns.push(`sexo = '${sexo}'`)
-            }if(idade){
-                setStatementCollumns.push(`idade = ${idade}`)
-            }if(descricao){
-                setStatementCollumns.push(`descricao = '${descricao}'`)
-            }
-
-            const sql = `UPDATE cadAnimal SET ${setStatementCollumns.join(",")} where id = ${id}`;
-            const query= connection.query(sql, function(error, results, fields){
-                if(error) throw error;
-                callback(results);
-            })
-    
-            console.log(query.sql);
-            connection.end();
-    
-        }
-
-        
-   
-
-
-    static  createAnimal(animal, callback){
-        const connection = CadAnimal.connect();
-        const {nome, especie, porte, sexo, idade, descricao} = animal;
-        
-       const sql = `INSERT INTO cadAnimal ( nome, especie, porte,sexo, idade, descricao ) values ( '${nome}', '${especie}', '${porte}', '${sexo}', ${idade}, '${descricao}')`; 
-        const query = connection.query(sql, function(error, results, fields){
-            if(error) throw error;
-        });
-
-        console.log(query.sql); 
-        connection.end();
-    }
-
-
+    console.log(query.sql);
+    connection.end();
+    callback();
+  }
 }
 
 module.exports = CadAnimal;
--- Routes/animalRoutes.js
+++ Routes/animalRoutes.js
@@ -1,41 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const CadAnimal = require('../Model/Animal')
+const CadAnimal = require('../Model/Animal');
 
-router.get('/home', function(req,res){
-    res.status(200).send('API da Casa Quatro Patas');
+router.get('/home', function (req, res) {
+  res.status(200).send('API da Casa Quatro Patas');
 });
 
 //busca todos os animais
-router.get('/', function(req,res){
-    CadAnimal.getAnimal(res)
-})
+router.get('/', function (req, res) {
+  CadAnimal.getAnimal(res);
+});
 
 //busca animal por id
-router.get('/:id', function(req,res){
-    const id = req.params.id;
-    CadAnimal.getAnimalById(id, function(animal){
-        res.json(animal)
-    })
-})
+router.get('/:id', function (req, res) {
+  const id = req.params.id;
+  CadAnimal.getAnimalById(id, function (animal) {
+    res.json(animal);
+  });
+});
 
 //post para salvar um animal
-router.post('/', function(req,res){
-    
-    const animal = req.body;
-    CadAnimal.createAnimal(animal, function(animal){
-        res.json({msg:"Animal inserido com sucesso"});
-    })
-})
+router.post('/', function (req, res) {
+  const animal = req.body;
+  CadAnimal.createAnimal(animal, function (animal) {
+    res.json({ msg: 'Animal inserido com sucesso' });
+  });
+});
 //put para atualizar um animal
-router.put('/:id', function(req,res){
-    const id = req.params.id;
-    const {nome, especie, porte, sexo, idade, descricao} = req.body;
+router.put('/:id', function (req, res) {
+  const id = req.params.id;
+  const { nome, especie, porte, sexo, idade } = req.body;
 
-    CadAnimal.updateAnimal(id, {nome, especie, porte, sexo, idade, descricao}, function(){
-        res.json({msg:"Registro atualizado com sucesso!"});
-    })
-
+  CadAnimal.updateAnimal(id, { nome, especie, porte, sexo, idade }, res);
 });
 
 module.exports = router;
